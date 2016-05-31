2bb7955429c05cf4074dea84cb7fbb5032fc78f9
Raghav Nagaraj <Raghavendra_Nagaraj@comcast.com>
2016-05-30 23:15:54 -0400

    latest changes

--- about.php
+++ about.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Srimad Solutions HTML5 Template | About Style One</title>
+<title>Srimad Solutions | About Us</title>
 <!-- Stylesheets -->
 <link href="css/bootstrap.css" rel="stylesheet">
 <link href="css/style.css" rel="stylesheet">
